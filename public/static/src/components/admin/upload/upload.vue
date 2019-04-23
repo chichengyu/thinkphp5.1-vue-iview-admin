74d3fd0fd74b458a190c58d398a51ac10e2d4592
xiaochi <q8752674252qq.com>
2019-04-23 13:58:37 +0800

    增加upload上传组件

---
+++ public/static/src/components/admin/upload/upload.vue
@@ -0,0 +1,131 @@
+<template>
+    <div class="upload">
+        <div class="demo-upload-list" v-for="item in fileImageList">
+            <template v-if="item.status === 'finished'">
+                <img :src="item.url">
+                <div class="demo-upload-list-cover">
+                    <Icon type="ios-eye-outline" @click.native="handleView(item.url)"></Icon>
+                    <Icon type="ios-trash-outline" @click.native="handleRemove(item)"></Icon>
+                </div>
+            </template>
+            <template v-else>
+                <Progress v-if="item.showProgress" :percent="item.percentage" hide-info></Progress>
+            </template>
+        </div>
+        <Upload
+                ref="upload"
+                :show-upload-list="false"
+                :default-file-list="defaultList"
+                :on-success="handleSuccess"
+                :format="['jpg','jpeg','png']"
+                :max-size="2048"
+                :on-format-error="handleFormatError"
+                :on-exceeded-size="handleMaxSize"
+                :before-upload="handleBeforeUpload"
+                type="drag"
+                :action="uploadUrl"
+                style="display: inline-block;width:58px;">
+            <div style="width: 58px;height:58px;line-height: 58px;">
+                <Icon type="ios-camera" size="20"></Icon>
+            </div>
+        </Upload>
+        <Modal title="View Image" v-model="visible">
+            <img :src="imgName" v-if="visible" style="width: 100%">
+        </Modal>
+    </div>
+</template>
+<script>
+    export default {
+        props:['uploadUrl'],
+        data () {
+            return {
+                defaultList: [],
+                imgName: '',
+                visible: false,
+                uploadList: [],
+                fileImageList:[]
+            }
+        },
+        methods: {
+            handleView (name) {
+                this.imgName = name;
+                this.visible = true;
+            },
+            handleRemove (file) {
+                // const fileList = this.$refs.upload.fileList;
+                // this.$refs.upload.fileList.splice(fileList.indexOf(file), 1);
+                this.fileImageList.splice(this.fileImageList.indexOf(file),1);
+            },
+            handleSuccess (res, file) {
+                if (res.code == 1){
+                    file.url = res.ivew_path;
+                    file.name = file.name;
+                    this.fileImageList.push(file)
+                    this.$emit('changeImageUrl',res.path);
+                }
+            },
+            delImageUlr(){
+                this.fileImageList = [];
+            },
+            handleFormatError (file) {
+                this.$Notice.warning({
+                    title: '图片格式错误',
+                    desc: 'File format of ' + file.name + ' is incorrect, please select jpg or png.'
+                });
+            },
+            handleMaxSize (file) {
+                this.$Notice.warning({
+                    title: '图片超过2M',
+                    desc: 'File  ' + file.name + ' is too large, no more than 2M.'
+                });
+            },
+            handleBeforeUpload () {
+                const check = this.uploadList.length < 5;
+                if (!check) {
+                    this.$Notice.warning({
+                        title: 'Up to five pictures can be uploaded.'
+                    });
+                }
+                return check;
+            }
+        }
+    }
+</script>
+<style scoped>
+    .demo-upload-list{
+        display: inline-block;
+        width: 60px;
+        height: 60px;
+        text-align: center;
+        line-height: 60px;
+        border: 1px solid transparent;
+        border-radius: 4px;
+        overflow: hidden;
+        background: #fff;
+        position: relative;
+        box-shadow: 0 1px 1px rgba(0,0,0,.2);
+        margin-right: 4px;
+    }
+    .demo-upload-list img{
+        width: 100%;
+        height: 100%;
+    }
+    .demo-upload-list-cover{
+        display: none;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background: rgba(0,0,0,.6);
+    }
+    .demo-upload-list:hover .demo-upload-list-cover{
+        display: block;
+    }
+    .demo-upload-list-cover i{
+        color: #fff;
+        font-size: 20px;
+        cursor: pointer;
+        margin: 0 2px;
+    }
+</style>
